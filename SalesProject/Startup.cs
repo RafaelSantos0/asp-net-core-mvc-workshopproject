b33fba15e2af36100573238d35227c411df15622
Rafael Santos <rafael.antunessantos@outlook.com>
2019-05-17 20:29:55 -0300

    DB

--- SalesProject/Startup.cs
+++ SalesProject/Startup.cs
@@ -37,7 +37,8 @@ namespace SalesProject
             services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 
             services.AddDbContext<SalesProjectContext>(options =>
-                    options.UseSqlServer(Configuration.GetConnectionString("SalesProjectContext")));
+                    options.UseMySql(Configuration.GetConnectionString("SalesProjectContext"), builder => 
+                     builder.MigrationsAssembly("SalesProject")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
