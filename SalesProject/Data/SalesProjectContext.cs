3e848c2977e500b6fce2e863a7cdd171bf1d2463
Rafael Santos <rafael.antunessantos@outlook.com>
2019-05-17 19:59:14 -0300

    CRUD scaffolding

---
+++ SalesProject/Data/SalesProjectContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace SalesProject.Models
+{
+    public class SalesProjectContext : DbContext
+    {
+        public SalesProjectContext (DbContextOptions<SalesProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<SalesProject.Models.Department> Department { get; set; }
+    }
+}
